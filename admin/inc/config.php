91fae4192b406f396c351d4b2701ad6611aec3cc
Shashank Sajjan <spsajjan7016@gmail.com>
2025-09-13 12:08:12 +0530

    Update

--- admin/inc/config.php
+++ admin/inc/config.php
@@ -3,7 +3,7 @@
 ini_set('error_reporting', E_ALL);
 
 // Setting up the time zone
-date_default_timezone_set('America/Los_Angeles');
+date_default_timezone_set('Asia/Kolkata');
 
 // Host Name
 $dbhost = 'localhost';
